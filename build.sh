e3629018b5684b0d50827ab39f173648534fba71
trx3030 <tttrrrxxx30@gmail.com>
2025-07-23 09:33:00 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# تحديث pip وأدوات البناء
-pip install --upgrade pip setuptools wheel
-
-# تثبيت المتطلبات
 pip install -r requirements.txt
-
-# تثبيت playwright وتهيئة المتصفح
-playwright install --with-deps
+playwright install
